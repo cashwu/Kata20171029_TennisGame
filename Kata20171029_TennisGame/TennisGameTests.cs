aac3d8626579578a477de9157957f49b4234858b
cash <cash@cashwu.com>
2017-10-29 23:33:25 +0800

    Love All

--- Kata20171029_TennisGame/TennisGameTests.cs
+++ Kata20171029_TennisGame/TennisGameTests.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20171029_TennisGame
 {
     [TestClass]
     public class TennisGameTests
     {
+        private TennisGame tennisGame = new TennisGame();
+
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            AssertScoreShouldBe("Love All");
+        }
+
+        private void AssertScoreShouldBe(string expected)
+        {
+            string score = tennisGame.Score();
+            Assert.AreEqual(expected, score);
+        }
+    }
+
+    public class TennisGame
+    {
+        public string Score()
+        {
+            return "Love All";
         }
     }
 }
